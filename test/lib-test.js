59d61b11b6642b499f645eed4c621af5e940010a
Alex LaFroscia <alex@lafroscia.com>
2020-03-25 11:21:32 -0400

    chore: update linting config

--- test/lib-test.js
+++ test/lib-test.js
@@ -11,35 +11,40 @@ function fixtureFiles(...names) {
   return names.map((fileName) => resolve(__dirname, './fixtures', fileName));
 }
 
-describe('merge logic', function() {
-  it('merges multiple YAML files', function() {
+describe('merge logic', function () {
+  it('merges multiple YAML files', function () {
     const output = merge(...fixtureFiles('basic/a.yml', 'basic/b.yml'));
 
-    expect(output).to.equal(stripIndent`
+    expect(output).to.equal(
+      stripIndent`
       a:
         foo: bar
       b:
         foo: bar
-    ` + '\n');
+    ` + '\n'
+    );
   });
 
-  it('overwrites properties from left to right', function() {
+  it('overwrites properties from left to right', function () {
     const output = merge(...fixtureFiles('overwrite/a.yml', 'overwrite/b.yml', 'overwrite/c.yml'));
 
-    expect(output).to.equal(stripIndent`
+    expect(output).to.equal(
+      stripIndent`
       key:
         value: c
-    ` + '\n');
+    ` + '\n'
+    );
   });
 
   it('allows keys to trickle in from all merge sources', function () {
     const output = merge(...fixtureFiles('merge/a.yml', 'merge/b.yml'));
-    
-    expect(output).to.equal(stripIndent`
+
+    expect(output).to.equal(
+      stripIndent`
       key:
         first_value: a
         second_value: b
-    ` + '\n');
+    ` + '\n'
+    );
   });
 });
-
